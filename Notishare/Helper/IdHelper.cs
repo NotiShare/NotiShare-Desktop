da6c14ec661d7f7116225dc130a11f8fb34b2a1f
FezZHD <zhenya535@gmail.com>
2017-05-14 19:19:00 +0300

    adding websocket object

---
+++ Notishare/Helper/IdHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+
+namespace Notishare.Helper
+{
+    internal static class IdHelper
+    {
+
+        internal static string GetDeviceId()
+        {
+            var cpuId = string.Empty;
+            try
+            {
+                ManagementObjectSearcher mbs = new ManagementObjectSearcher("Select ProcessorID From Win32_processor");
+                ManagementObjectCollection mbsList = mbs.Get();
+
+                foreach (var mo in mbsList)
+                {
+                    cpuId = mo["ProcessorID"].ToString();
+                }
+                return cpuId;
+            }
+            catch (Exception) { return cpuId; }
+        }
+    }
+}
---
+++ Notishare/Model/HttpWorker/WebSocket.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpSocket = WebSocketSharp;
+
+namespace Notishare.Model.HttpWorker
+{
+    internal class WebSocket
+    {
+
+        private const string DefaultUrl = "ws://192.168.100.6";
+
+        private string url;
+
+        private int port;
+
+        private string id;
+
+        private string deviceDbId;
+
+        private string userDbId;
+
+        private string deviceType;
+
+        private SharpSocket.WebSocket socket;
+
+        private Action<string> sendAction;
+
+        public WebSocket(string url, int port, string id, string deviceDbId, string userDbId, string deviceType, Action<string> sendAction)
+        {
+            this.url = url;
+            this.port = port;
+            this.id = id;
+            this.deviceDbId = deviceDbId;
+            this.userDbId = userDbId;
+            this.deviceType = deviceType;
+            this.sendAction = sendAction;
+        }
+
+
+
+        public void Init()
+        {
+
+            socket = new SharpSocket.WebSocket($"{DefaultUrl}:{port}/{url}?id={id}&deviceId={deviceDbId}&userId={userDbId}&type={deviceType}");
+            socket.OnOpen += SocketOnOnOpen;
+            socket.OnMessage += SocketOnOnMessage;
+            socket.OnError += SocketOnOnError;
+            socket.OnClose += SocketOnOnClose;
+            socket.Connect();
+        }
+
+        private void SocketOnOnClose(object sender, SharpSocket.CloseEventArgs closeEventArgs)
+        {
+            
+        }
+
+        private void SocketOnOnError(object sender, SharpSocket.ErrorEventArgs errorEventArgs)
+        {
+           
+        }
+
+        private void SocketOnOnMessage(object sender, SharpSocket.MessageEventArgs messageEventArgs)
+        {
+            sendAction.Invoke(messageEventArgs.Data);
+        }
+
+        private void SocketOnOnOpen(object sender, EventArgs eventArgs)
+        {
+            
+        }
+
+
+        public void SendData(string message)
+        {
+            socket.Send(message);
+        }
+
+
+        public void Close()
+        {
+            socket.Close();
+        }
+    }
+}
